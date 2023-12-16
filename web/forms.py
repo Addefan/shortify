68cd1337d9578b69c78f4189b50b54c84f2bab48
Vitaly Zorin <addefan@mail.ru>
2023-12-16 04:12:17 +0300

    + form and template for creating link

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.forms import ModelForm
+
+from web.models import Link
+from web.service import generate_short_link
 
 
 class BootstrapFormMixin:
@@ -40,3 +44,29 @@ class ProfileForm(BootstrapFormMixin, UserChangeForm):
     class Meta(UserChangeForm.Meta):
         fields = ("first_name", "last_name", "email", "username")
         help_texts = {"username": None}
+
+
+class LinkCreationForm(BootstrapFormMixin, ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        if not self.user.is_anonymous:
+            self.instance.user = self.user
+
+        short_link = generate_short_link(self.instance.original_absolute_url)
+        while Link.objects.filter(short_relative_url=short_link).count():
+            short_link = generate_short_link(self.instance.original_absolute_url)
+        self.instance.short_relative_url = short_link
+
+        if commit:
+            instance.save()
+
+        return instance
+
+    class Meta:
+        model = Link
+        fields = ("original_absolute_url", "is_public")
