f7c6239428ccdb36c81f528c90e16207d244d3d2
Vitaly Zorin <addefan@mail.ru>
2023-12-17 03:02:51 +0300

    + link factory

--- web/factories.py
+++ web/factories.py
@@ -1,6 +1,9 @@
 import factory
 
-from web.models import User
+from web.models import User, Link
+from django.utils.timezone import now
+
+from web.services import generate_short_link
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -12,3 +15,16 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = User
+
+
+class LinkFactory(factory.django.DjangoModelFactory):
+    has_user = factory.Faker("pybool")
+    user = factory.Maybe("has_user", factory.SubFactory(User), None)
+    original_absolute_url = factory.Faker("url")
+    short_relative_url = factory.LazyFunction(generate_short_link)
+    is_public = factory.Faker("pybool")
+    created_at = factory.LazyFunction(now)
+
+    class Meta:
+        model = Link
+        exclude = ("has_user",)
