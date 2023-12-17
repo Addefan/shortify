1e75789df681fb6d878da377cf60a85e351c554c
Vitaly Zorin <addefan@mail.ru>
2023-12-17 04:28:37 +0300

    = generate data without bulk

--- web/management/commands/generate_test_data.py
+++ web/management/commands/generate_test_data.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 
 from web.factories import VisitFactory
-from web.models import Visit, User, Link
 
 
 class Command(BaseCommand):
@@ -10,11 +10,13 @@ class Command(BaseCommand):
                             help="Количество объектов Visit для генерирования (по умолчанию 1000).")
 
     def handle(self, *args, **options):
-        visits = [VisitFactory.build() for _ in range(options["amount"])]
-        links = [visit.link for visit in visits]
-        users = [link.user for link in links if link.user is not None]
+        count = 0
 
-        users = User.objects.bulk_create(users, ignore_conflicts=True)
-        links = Link.objects.bulk_create(links, ignore_conflicts=True)
-        visits = Visit.objects.bulk_create(visits, ignore_conflicts=True)
-        print(f"Успешно создано {len(users)} пользователей, {len(links)} ссылок и {len(visits)} посещений.")
+        for _ in range(options["amount"]):
+            try:
+                VisitFactory.create()
+                count += 1
+            except IntegrityError:
+                pass
+
+        print(f"Успешно создано {count} посещений.")
